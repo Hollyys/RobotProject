d488bf643eaa8e01806be0729655c0092e7ef9a5
Sunghan Shin <crossrunway01@gmail.com>
2024-05-08 00:13:48 +0900

    feat: 리액트에서 이미지 입력 받아서 미리보기 출력하는 기능 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,43 @@
+import React, { useState } from 'react';
 import image from './knu.png'
 import './App.css';
 
 function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setSelectedImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={image} width = '800px' />
+        <img src={image} width="600px" alt="KNU Logo" />
         <p>
           모바일 로봇 프로그래밍 중간과제
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Hollyys/RobotProject.git"
           target="_blank"
           rel="noopener noreferrer"
+          style={{ marginTop: '20px', display: 'block' }} // 하이퍼링크에 간격 추가
         >
-          Learn React
+          git repository
         </a>
+        {selectedImage && (
+          <img src={selectedImage} alt="Uploaded" style={{ width: '800px', marginTop: '20px' }} />
+        )}
+        <div style={{ marginTop: '20px' }}>
+          <input type="file" onChange={handleImageChange} />
+        </div>
       </header>
     </div>
   );
