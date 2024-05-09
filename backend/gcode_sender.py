0a40c19d9092a2ba3e388c15757b73ec00297419
Hollyys <crossrunway01@gmail.com>
2024-05-09 16:14:14 +0900

    feat: 로봇의 응답에 따라 request를 보내는 기능 추가

--- backend/gcode_sender.py
+++ backend/gcode_sender.py
@@ -1,9 +1,14 @@
 import requests
+import time
 
 def sender(file_path):
     url = 'http://192.168.120.36/command?commandText='
     with open(file_path, 'r') as file:
         for line in file:
             if line[0] != '%':
-                command = url + line
-                # print(command)
+                response = 'busy'
+                while response != 'ok':
+                    command = url + line
+                    print(command)
+                    response = requests.post(url)
+                    time.sleep(1)
